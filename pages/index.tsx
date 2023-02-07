c40f80eb9960a7c039cbfbb9e1e26ac2a2a4105c
Luis Azocar <lazocar.dev@gmail.com>
2023-02-07 08:51:48 -0300

    dark theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from "next";
+import { Button } from "@nextui-org/react";
 
 const HomePage: NextPage = () => {
+  const Component = () => <Button>Click me</Button>;
   return (
     <>
+      {<Component />}
       <h1>Hola Mundo</h1>
     </>
   );
